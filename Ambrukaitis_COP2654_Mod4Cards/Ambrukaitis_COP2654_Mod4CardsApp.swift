3dc9fb0db5f564b54c4d17ad00f518f8aef2069e
Matas Ambrukaitis <mambrukaitis@icloud.com>
2025-02-09 13:18:59 -0500

    Initial Commit

---
+++ Ambrukaitis_COP2654_Mod4Cards/Ambrukaitis_COP2654_Mod4CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ambrukaitis_COP2654_Mod4CardsApp.swift
+//  Ambrukaitis_COP2654_Mod4Cards
+//
+//  Created by Matas Ambrukaitis on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Ambrukaitis_COP2654_Mod4CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ambrukaitis_COP2654_Mod4Cards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ambrukaitis_COP2654_Mod4Cards
+//
+//  Created by Matas Ambrukaitis on 2/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
