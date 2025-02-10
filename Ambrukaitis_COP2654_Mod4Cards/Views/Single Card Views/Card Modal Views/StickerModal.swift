2d9e17d423bd74c05b9b1724ea7f3da2527a414b
Matas Ambrukaitis <mambrukaitis@icloud.com>
2025-02-09 21:35:59 -0500

    Added the ability to add stickers to the cards.

---
+++ Ambrukaitis_COP2654_Mod4Cards/Views/Single Card Views/Card Modal Views/StickerModal.swift
@@ -0,0 +1,18 @@
+//
+//  StickerModal.swift
+//  Ambrukaitis_COP2654_Mod4Cards
+//
+//  Created by Matas Ambrukaitis on 2/9/25.
+//
+
+import SwiftUI
+
+struct StickerModal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StickerModal()
+}
